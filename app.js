0bcab0e925823a76e83be65b6d4fb4b5c6b03194
Aldrin <aldrinmcaalim@gmail.com>
2021-04-03 23:23:06 -0400

    Created pseudodata and basic skeleton of HTML

---
+++ app.js
@@ -0,0 +1,52 @@
+const blitzChess = [
+  {
+    blackPieces: {
+      player: "Magnus Carlsen",
+      isWinner: true,
+    },
+    whitePieces: {
+      player: "Hikaru Nakamura",
+      isWinner: false,
+    },
+  },
+  {
+    blackPieces: {
+      player: "Hikaru Nakamura",
+      isWinner: false,
+    },
+    whitePieces: {
+      player: "Magnus Carlsen",
+      iswinner: true,
+    },
+  },
+  {
+    blackPieces: {
+      player: "Magnus Carlsen",
+      isWinner: false,
+    },
+    whitePieces: {
+      player: "Hikaru Nakamura",
+      isWinner: true,
+    },
+  },
+  {
+    blackPieces: {
+      player: "Hikaru Nakamura",
+      isWinner: true,
+    },
+    whitePieces: {
+      player: "Magnus Carlsen",
+      isWinner: false,
+    },
+  },
+  {
+    blackPieces: {
+      player: "Magnus Carlsen",
+      isWinner: true,
+    },
+    whitePieces: {
+      player: "Hikaru Nakamura",
+      isWinner: false,
+    },
+  },
+];
