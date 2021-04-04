f5f6c55d89cc87d29f3cac1c20c7674b00f93ff9
Aldrin <aldrinmcaalim@gmail.com>
2021-04-03 23:48:19 -0400

    Display information using JS
    Using the pseudoData, you can see the players. Will work on displaying who won and lost

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const blitzChess = [
+const blitzGames = [
   {
     blackPieces: {
       player: "Magnus Carlsen",
@@ -16,7 +16,7 @@ const blitzChess = [
     },
     whitePieces: {
       player: "Magnus Carlsen",
-      iswinner: true,
+      isWinner: true,
     },
   },
   {
@@ -50,3 +50,30 @@ const blitzChess = [
     },
   },
 ];
+
+console.log(blitzGames[0].blackPieces.player);
+console.log(blitzGames[0].blackPieces.isWinner);
+
+const body = document.body;
+const parentUl = document.createElement("ul");
+body.append(parentUl);
+
+// childLi.innerText = "Magnus Carlsen vs. Hikaru Nakamura win-lose";
+// parentUl.append(childLi);
+
+for (let blitzGame of blitzGames) {
+  // Creates a new li child to append onto the document after every loop
+  const childLi = document.createElement("li");
+
+  // Removes the bullet from the li after every loop
+  childLi.style.listStyleType = "none";
+  // deconstructs the information from the object
+  const { blackPieces: black } = blitzGame;
+  const { whitePieces: white } = blitzGame;
+  const { player: bPlayer } = black;
+  const { player: wPlayer } = white;
+
+  // Uses the information we have gathered to display on the HTML document
+  childLi.innerText = `${bPlayer} vs. ${wPlayer}`;
+  parentUl.append(childLi);
+}
