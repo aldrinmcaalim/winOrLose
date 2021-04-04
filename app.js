8c7da87ffe4dcd2722f94e7563a3248b0477a591
Aldrin <aldrinmcaalim@gmail.com>
2021-04-04 00:07:10 -0400

    Created basic functionality
    Displaying the players, the background color for who won on black side

--- app.js
+++ app.js
@@ -1,50 +1,60 @@
 const blitzGames = [
   {
     blackPieces: {
+      color: "black",
       player: "Magnus Carlsen",
       isWinner: true,
     },
     whitePieces: {
+      color: "white",
       player: "Hikaru Nakamura",
       isWinner: false,
     },
   },
   {
     blackPieces: {
+      color: "black",
       player: "Hikaru Nakamura",
       isWinner: false,
     },
     whitePieces: {
+      color: "white",
       player: "Magnus Carlsen",
       isWinner: true,
     },
   },
   {
     blackPieces: {
+      color: "black",
       player: "Magnus Carlsen",
       isWinner: false,
     },
     whitePieces: {
+      color: "white",
       player: "Hikaru Nakamura",
       isWinner: true,
     },
   },
   {
     blackPieces: {
+      color: "black",
       player: "Hikaru Nakamura",
       isWinner: true,
     },
     whitePieces: {
+      color: "white",
       player: "Magnus Carlsen",
       isWinner: false,
     },
   },
   {
     blackPieces: {
+      color: "black",
       player: "Magnus Carlsen",
       isWinner: true,
     },
     whitePieces: {
+      color: "white",
       player: "Hikaru Nakamura",
       isWinner: false,
     },
@@ -70,10 +80,18 @@ for (let blitzGame of blitzGames) {
   // deconstructs the information from the object
   const { blackPieces: black } = blitzGame;
   const { whitePieces: white } = blitzGame;
-  const { player: bPlayer } = black;
-  const { player: wPlayer } = white;
+  const { player: bPlayer, color: bColor, isWinner: bWinner } = black;
+  const { player: wPlayer, color: wColor, isWinner: wWinner } = white;
 
+  if (bWinner) {
+    childLi.classList.add("winner");
+    childLi.classList.remove("loser");
+  } else {
+    childLi.classList.add("loser");
+    childLi.classList.remove("winner");
+  }
+  //   childLi.style.backgroundColor = "red";
   // Uses the information we have gathered to display on the HTML document
-  childLi.innerText = `${bPlayer} vs. ${wPlayer}`;
+  childLi.innerText = `${bColor} vs. ${wColor} ${bPlayer} vs. ${wPlayer}`;
   parentUl.append(childLi);
 }
